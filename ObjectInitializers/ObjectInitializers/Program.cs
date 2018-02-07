6bb3000d1f071be0a4dcd2deac59909e868fcb46
Gleb999 <lihachev99gleb@mail.ru>
2018-02-07 13:34:17 +0700

    traditional approach

--- ObjectInitializers/ObjectInitializers/Program.cs
+++ ObjectInitializers/ObjectInitializers/Program.cs
@@ -12,23 +12,18 @@ namespace ObjectInitializers
         {
             Console.WriteLine("***** Fun with Object Init Syntax *****\n");
 
-            Point firstPoint = new Point();
-            firstPoint.X = 10;
-            firstPoint.Y = 10;
-            firstPoint.DisplayStats();
 
-            Point anotherPoint = new Point(20, 20);
-            anotherPoint.DisplayStats();
+            Rectangle r = new Rectangle();
+            Point p1 = new Point();
 
-            Point finalPoint = new Point { X = 30, Y = 30 };
-            finalPoint.DisplayStats();
+            p1.X = 10;
+            p1.Y = 10;
 
-            Rectangle myRect = new Rectangle
-            {
-                TopLeft = new Point { X = 10, Y = 10 },
-                BottomRight = new Point { X = 200, Y = 200 }
-            };
-
+            r.TopLeft = p1;
+            Point p2 = new Point();
+            p2.X = 200;
+            p2.Y = 200;
+            r.BottomRight = p2;
             Console.ReadLine();
         }
     }
