72f20954fee639abfe223e698a255558781f6e6c
Gleb999 <lihachev99gleb@mail.ru>
2018-02-07 13:15:49 +0700

    object initialization syntax

---
+++ ObjectInitializers/ObjectInitializers/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xVal, int uVal)
+        {
+            X = xVal;
+            Y = xVal;
+        }
+
+        public Point() { }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("[{0}, {1}]", X, Y);
+        }
+    }
+}
